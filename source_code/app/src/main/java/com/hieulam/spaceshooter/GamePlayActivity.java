cd3a4d775d9505b1a733d07afe482d9ee1c04526
Nguyen Trong Hieu <tronghieu8531@gmail.com>
2019-11-22 17:21:56 +0700

    + Add spaceship resource

--- source_code/app/src/main/java/com/hieulam/spaceshooter/GamePlayActivity.java
+++ source_code/app/src/main/java/com/hieulam/spaceshooter/GamePlayActivity.java
@@ -15,7 +15,6 @@ public class GamePlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         init();
     }
 
--- source_code/app/src/main/java/com/hieulam/spaceshooter/MainActivity.java
+++ source_code/app/src/main/java/com/hieulam/spaceshooter/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // Apply activity transition
